ec4390deed703c94efd70f530a964672bcdc45c2
“Your <shakil3300bd@gmail.com>
2022-01-08 22:51:35 +0600

    [Methode and Debugging]

--- Loop Condition and Convertion/Loop Condition and Convertion/Program.cs
+++ Loop Condition and Convertion/Loop Condition and Convertion/Program.cs
@@ -63,6 +63,8 @@ namespace ConditionalStatement
                 Console.WriteLine("Hello World");
             }
 
+
+
         }
     }
 }
---
+++ Methode and Debuge/Methode and Debuge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingMethodeDebugg
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+           int result= Add(10, 20);
+            Console.WriteLine("Result : "+result);
+        }
+
+        static int Add(int firstNumber, int secondNumber)
+        {
+            int sum=firstNumber+secondNumber;
+            return sum;
+        }
+    }
+}
